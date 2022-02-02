f76c41f6ea9ed657d387df83506c235dae602739
jaglyder <98897498+jaglyder@users.noreply.github.com>
2022-02-02 18:46:25 -0500

    MIS 310
    HTML/ CSS/ JS

---
+++ assignment/madlib.js.css
@@ -0,0 +1,65 @@
+// all the javascript/Jquery is wrapped in this Jquery ready call back function that is attached to the DOM so that is won't be processed or executed until the document is fully loaded
+$(document).ready(function(){
+  
+  var waggoner = ['Sailor', 'Driver', 'Diver', 'Philosopher', 'Teacher'];
+  var sank = ['fell', 'floated', 'exploded', 'flew', 'dropped'];
+  var heavy = ['light', 'dense', 'buoyant', 'flimsy', 'loose'];
+  var strong = ['Weak', 'Frail', 'Puny', 'Stupid', 'Floppy', 'Loser'];
+  var hercules = ['Bob', 'Apollo', 'Poseidon', 'Hades', 'Donald Trump'];
+  var dog = ['horse', 'zebra', 'cat', 'moose', 'tiger', 'llama'];
+  var piece = ['chunk', 'portion', 'slab', 'slice', 'ball'];
+  var meat = ['corn', 'salad', 'chicken', 'beef', 'potato'];
+  var eat = ['barf', 'baptize', 'lick', 'punch', 'swallow'];
+  var running = ['streaming', 'babbling', 'frozen', 'shallow', 'deep'];
+
+  $(".waggoner").click(function(e){
+    e.stopPropagation();
+    $(".waggoner").text(waggoner[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".sank").click(function(e){
+    e.stopPropagation();
+    $(".sank").text(sank[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".heavy").click(function(e){
+    e.stopPropagation();
+    $(".heavy").text(heavy[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".strong").click(function(e){
+    e.stopPropagation();
+    $(".strong").text(strong[Math.floor(Math.random() * 6)]);
+  });
+
+  $(".hercules").click(function(e){
+    e.stopPropagation();
+    $(".hercules").text(hercules[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".dog").click(function(e){
+    e.stopPropagation();
+    $(".dog").text(dog[Math.floor(Math.random() * 6)]);
+  });
+
+  $(".piece").click(function(e){
+    e.stopPropagation();
+    $(".piece").text(piece[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".meat").click(function(e){
+    e.stopPropagation();
+    $(".meat").text(meat[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".eat").click(function(e){
+    e.stopPropagation();
+    $(".eat").text(eat[Math.floor(Math.random() * 5)]);
+  });
+
+  $(".running").click(function(e){
+    e.stopPropagation();
+    $(".running").text(running[Math.floor(Math.random() * 5)]);
+  });
+
+});
